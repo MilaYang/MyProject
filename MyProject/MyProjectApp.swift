e6b5bec88686b8a65a8e016a7afd729e6f2bca87
mila <milayang92@gmail.com>
2023-01-06 21:31:54 +0900

    first commit

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by Mila on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
